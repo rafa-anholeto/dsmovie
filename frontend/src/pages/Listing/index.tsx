126180fddb2265b8a24e88ec754493e53f01634f
rafa-anholeto <rafaanholeto3@gmail.com>
2022-01-14 09:12:11 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,19 +1,25 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
 
-  // FORMA ERRADA
-  axios.get(`${BASE_URL}/movies?size=12&page=0`)
-      .then(response => {
-          console.log(response.data);
-      });
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=1`).then((response) => {
+      const data = response.data as MoviePage;
+      console.log(data);
+      setPageNumber(data.number);
+    });
+  }, []);
 
 
   return (
     <>
+      <p>{pageNumber}</p>
       <Pagination />
 
       <div className="container">
@@ -33,7 +39,6 @@ function Listing() {
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-          
         </div>
       </div>
     </>
